b9ac042cc10dea88d193374ebb8c9e28da051fbb
Sebastien100 <129985367+Sebastien100@users.noreply.github.com>
2024-04-13 18:13:22 -0400

    put API key

--- client/src/client.js
+++ client/src/client.js
@@ -1,5 +1,5 @@
 import {createClient} from '@supabase/supabase-js'
 const URL = 'https://igsgczcwugnszlufrhmg.supabase.co'
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlnc2djemN3dWduc3psdWZyaG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4ODgyOTYsImV4cCI6MjAyODQ2NDI5Nn0.OqSuvkVGw0mbkduziPbZsQDiYQr25joCYJqFeVO037I'
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlnc2djemN3dWduc3psdWZyaG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4ODgyOTYsImV4cCI6MjAyODQ2NDI5Nn0.OqSuvkVGw0mbkduziPbZsQDiYQr25joCYJqFeVO037I'
 
 export const supabase = createClient(URL, API_KEY)
